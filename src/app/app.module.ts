d30baa02b0e9eedc624e59544920ece9b0b950a4
Yassine Mabrouk <y.mabrouk@slokware.com>
2021-08-22 20:50:31 +0100

    ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      CoursesComponent
+   ],
   imports: [
     BrowserModule
   ],
---
+++ src/app/courses/courses.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+    
+  public courses=["react","anagular","VueJs"];
+  detailInfo:boolean=false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  detail($event){
+  this.detailInfo=!this.detailInfo;
+  console.log(this.detailInfo)
+  $event.stopPropagation();
+  }
+}
