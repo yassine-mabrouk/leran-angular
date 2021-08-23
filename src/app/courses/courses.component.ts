c014b5ce3d026c0592d7ea45f501db22809425d9
Yassine Mabrouk <y.mabrouk@slokware.com>
2021-08-23 01:16:33 +0100

    ngFor

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -22,4 +22,29 @@ export class CoursesComponent implements OnInit {
   }
   // ngSwitch 
   viewFram="angular";
+  // ngFor 
+  frams;
+  deleteFram(course){
+     
+     let index=this.frams.indexOf(course);
+     this.frams.splice(index,1);
+     console.log("delete runing "+index);
+  }
+  // tarckBy : pour ne pas recuperer data doublement
+  loadCourses() {
+  this.frams=[
+    {id:1,title:"Angular"},
+    {id:2,title:"React"},
+    {id:3,title:"Spring boot"},
+    {id:4,title:"laravel"},
+    {id:5,title:"nodeJs"},
+  ]
+  }
+    // tarckBy : pour ne pas recuperer data doublement
+    // faire le track By Id 
+
+    trackFrams (index , course ){
+      return course? course.id: undefined;
+    }
+
 }
